ae5399a3959440db187db2df54163cbd8c0f4bc1
Alexz330 <72354711+Alexz330@users.noreply.github.com>
2020-11-06 02:17:44 -0600

    ....

--- app.py
+++ app.py
@@ -1,22 +1,34 @@
 from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, InputRequired, Email, Length
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+
 
 from flask_sqlalchemy import SQLAlchemy
-import sys
 
-
+from flask_mysqldb import MySQL
+import bcrypt
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///DataBase.db"
 
-db = SQLAlchemy(app) 
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'hello'
+
+
+
+# Mysql Connection
+app.config['MYSQL_HOST'] = 'localhost' 
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'usuarios'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
+
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///DataBase.db"
+
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///DataBase.db"
+
+# db = SQLAlchemy(app) 
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+# login_manager.login_view = 'hello'
 
 
 bootstrap = Bootstrap(app)
@@ -24,32 +36,32 @@ bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), unique=True)
-    email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(80))
+# class User(UserMixin, db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(15), unique=True)
+#     email = db.Column(db.String(50), unique=True)
+#     password = db.Column(db.String(80))
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired(),Length(min=4, max=15)])
-    password = PasswordField('Password', validators=[DataRequired(),Length(min=4, max=10)])
-    remember = BooleanField('remember me')
-
-    submit = SubmitField('Enviar')
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.get(int(user_id))
 
 
 
-class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+# class LoginForm(FlaskForm):
+#     username = StringField('Nombre de usuario', validators=[DataRequired(),Length(min=4, max=15)])
+#     password = PasswordField('Password', validators=[DataRequired(),Length(min=4, max=10)])
+#     remember = BooleanField('remember me')
+
+#     submit = SubmitField('Enviar')
+
+
+
+# class RegisterForm(FlaskForm):
+#     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+#     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+#     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
 
 
@@ -75,7 +87,7 @@ def index():
 
 
 @app.route('/static/cargaMasiva.html')
-@login_required
+
 def cargaMasiva():
     return render_template('cargaMasiva.html')
 
@@ -90,10 +102,10 @@ def cartillas():
 
 
 @app.route('/static/perfil.html')
-@login_required
+
 
 def Perfil():
-    return render_template('perfil.html',name=current_user.username)
+    return render_template('perfil.html',)
 
 
 
@@ -107,70 +119,48 @@ def crearUsuario():
 
 @app.route('/login', methods=['GET', 'POST'])
 def hello():
-    
-    login_form = LoginForm()
-    username = session.get('username')
+     
 
-    context = {
-        
-        'login_form': login_form,
-        'username': username
-    }
+    return render_template('login.html')
 
-    if login_form.validate_on_submit():
-       user =User.query.filter_by(username=login_form.username.data).first()
-       if user:
-           if user.password == login_form.password.data:
-               login_user(user, remember=login_form.remember.data)
-               return render_template('perfil.html')  
+       # return'<h1>nombre de usuario o contraseña incorrecta</h1>'    
 
-           flash('Nombre de usuario o contraseña incorrecta') 
-                  
-    # return'<h1>nombre de usuario o contraseña incorrecta</h1>'    
 
-    
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template("Signup.html")
+    else:
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password'].encode('utf-8')
+        hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
 
-    return render_template('Login.html', **context,form=login_form)
-
-@app.route('/signup', methods=['GET', 'POST'])
-def singup():
-    form = RegisterForm()
-
-    if form.validate_on_submit():
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users (name, email, password) VALUES (%s,%s,%s)",(name,email,hash_password,))
+        mysql.connection.commit()
+        session['name'] = request.form['name']
+        session['email'] = request.form['email']
+        return redirect(url_for('index'))
+  
 
     
-        new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
-
-       
-        db.session.add(new_user)
-        db.session.commit()
-       # return '<h1>' + form.username.data +' ' +form.email.data + ' ' +form.password.data '</h1>'
-        return '<h1>New user has been created!</h1>'
-
-    return render_template('Signup.html', form = form)
 
 
 @app.route('/logout')
-@login_required
+
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
-    
 
 
 
 
 
-
-if __name__ == "__main__":
-    start_db = sys.argv[1] if len(sys.argv) > 1 else ""
-    db.create_all() if start_db == "start_db" else None
-    print("===== Database Created =====") if start_db == "start_db" else None
+if __name__ == '__main__':
+    app.secret_key = "^A%DJAJU^JJ123"
+    app.run(debug = True, port= 8000)# Se encarga de ejecutar el servidor 5000
 
 
-
-
-
-app.run(debug = True, port= 8000)# Se encarga de ejecutar el servidor 5000
 
